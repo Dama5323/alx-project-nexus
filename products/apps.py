c95bfd7efc5ea204adfe066377619ebb3eb4e563
Dama5323 <your-deenyashke@gmail.com>
2025-08-11 05:45:41 +0300

    fix(products): handle missing django_content_type table in AppConfig.ready()

--- products/apps.py
+++ products/apps.py
@@ -1,24 +1,26 @@
-# products/apps.py
 from django.apps import AppConfig
-from django.core.cache import cache
-from django.core.cache.backends.base import InvalidCacheBackendError
 
 class ProductsConfig(AppConfig):
     name = 'products'
 
     def ready(self):
         try:
+            from django.core.cache import cache
+            from django.core.cache.backends.base import InvalidCacheBackendError
+            from django.db.utils import OperationalError, ProgrammingError
+
             if not cache.get('jwt_content_types_cached'):
                 from django.contrib.contenttypes.models import ContentType
                 from django.apps import apps
-                
+
                 try:
                     ContentType.objects.get_for_models(
                         apps.get_model('token_blacklist', 'BlacklistedToken'),
-                        apps.get_model('token_blacklist', 'OutstandingToken')
+                        apps.get_model('token_blacklist', 'OutstandingToken'),
                     )
                     cache.set('jwt_content_types_cached', True, timeout=None)
-                except LookupError:
+                except (LookupError, OperationalError, ProgrammingError):
+                    # Tables might not exist yet
                     pass
         except (InvalidCacheBackendError, ConnectionError):
             # Silently fail if cache isn't available
