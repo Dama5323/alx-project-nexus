b29515c280eddfbf6f90b22a4b6f6b8c62bca108
Dama5323 <your-deenyashke@gmail.com>
2025-08-12 06:24:32 +0300

    fix(cart): resolve NoReverseMatch for 'cart-detail' URL

--- cart/tests/test_api.py
+++ cart/tests/test_api.py
@@ -1,17 +1,21 @@
-from rest_framework.test import APITestCase
+from django.test import TestCase
 from django.urls import reverse
-from cart.models import Cart, CartItem
+from cart.models import Cart
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class CartAPITest(APITestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(email='apiuser@example.com', password='testpass123')
-        self.client.login(email='apiuser@example.com', password='testpass123')
-        self.cart = Cart.objects.create(user=self.user)
+class CartViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            email='test@example.com',
+            password='testpass123'
+        )
+        cls.cart = Cart.objects.create(user=cls.user)
 
     def test_get_cart(self):
-        url = reverse('cart:cart-detail', kwargs={'pk': self.cart.pk})  # Added namespace
+        self.client.force_login(self.user)
+        url = reverse('cart:cart-detail', kwargs={'pk': self.cart.pk})
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
